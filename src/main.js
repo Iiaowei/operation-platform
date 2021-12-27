d4755fdef98b4a3da7393816672cca6d5e35bfad
LIAOWEI97 <lw993859222@gmail.com>
2021-12-27 22:37:11 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import TDesign from 'tdesign-vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import 'tdesign-vue/es/style/index.css'
+
+Vue.use(TDesign)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
